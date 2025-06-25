d40111bc43614d3fcb19a5c09d0325c022817e69
folakemi orekoya <orekoyafolakemi@gmail.com>
2025-06-25 16:05:36 +0100

    update the code to the original

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const cookieParser = require("cookie-parser"); 
 const dotenv = require("dotenv");
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,6 @@ const { env } = require("./config");
 
 const PORT = env.PORT;
 
-console.log('PORT from env:', PORT);
-console.log('Starting server...');
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);                                                                                                                                                
 });
